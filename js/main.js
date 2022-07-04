4c49b1362c9814297d5790647c2695bbf88b6b03
tomasfmarsico <tomasfmarsico@gmail.com>
2022-07-04 10:06:21 -0300

    segundo commit. Actualización de proyecto en relación a las últimas entregas

--- js/main.js
+++ js/main.js
@@ -8,80 +8,69 @@ class Producto {
 		this.id = id;
 		this.nombre = nombre;
 		this.precio = precio;
-		this.vendido = false;
 	}
 	resumen() {
-		console.log(
-			`El producto ${this.id}- ${this.nombre} cuesta $${this.precio}`
-		);
+		console.log(`El producto ${this.nombre} cuesta $${this.precio}`);
 	}
-	// sumaIva() {
-	// 	this.precio = this.precio * 1.21;
-	// 	pagarCarrito = parseInt(
-	// 		prompt(`El producto ${this.nombre} cuesta con IVA $${this.precio}`)
-	// 	);
-	// }
 }
 /** Arrays */
 const productos = [];
-const carrito = [];
 /** Push de productos al array "productos" */
 const producto1 = productos.push(new Producto("1", "Pikachu", "1300"));
 const producto2 = productos.push(new Producto("2", "Charmander", "1100"));
 const producto3 = productos.push(new Producto("3", "Espeon", "1000"));
 const producto4 = productos.push(new Producto("4", "Eevee", "980"));
-console.log(productos);
-
-/** Entrada para mostrar catálogo*/
-let entrada1 = parseInt(
-	prompt("Bienvenido\n ¿Quiere ver nuestro catálogo?\n 1(SI) 2(NO)")
-);
-
-if (entrada1 == 1) {
-	for (const producto of productos) {
-		producto.resumen();
-	}
-} else if (entrada1 == 2) {
-	alert("Gracias por su visita");
-} else {
-	alert("Debe ingresar una entrada válida");
-	entrada1 = parseInt(
-		prompt("Bienvenido\n ¿Quiere ver nuestro catálogo?\n 1(SI) 2(NO)")
-	);
+for (const producto of productos) {
+	producto.resumen();
 }
 
-/** Entrada para comprar producto */
-let entrada2 = parseInt(
-	prompt(
-		`¿Qué producto desea comprar?\n 1- ${productos[0].nombre} $${productos[0].precio}\n 2- ${productos[1].nombre} $${productos[1].precio}\n 3- ${productos[2].nombre} $${productos[2].precio}\n 4- ${productos[3].nombre} $${productos[3].precio}\n`
-	)
-);
+let articulosCarrito = [];
+let carritoHTML = document.getElementById("carritoHTML");
 
-if (entrada2 == 1) {
-	carrito.push = productos[0];
-	alert(
-		`Usted va a comprar 1 ${productos[0].nombre} cuyo precio es $${productos[0].precio}`
-	);
-	console.log(carrito);
-} else if (entrada2 == 2) {
-	carrito.push = productos[1];
-	alert(
-		`Usted va a comprar 1 ${productos[1].nombre} cuyo precio es $${productos[1].precio}`
-	);
-	console.log(carrito);
-} else if (entrada2 == 3) {
-	carrito.push = productos[2];
-	alert(
-		`Usted va a comprar 1 ${productos[2].nombre} cuyo precio es $${productos[2].precio}`
-	);
-	console.log(carrito);
-} else if (entrada2 == 4) {
-	carrito.push = productos[3];
-	alert(
-		`Usted va a comprar 1 ${productos[3].nombre} cuyo precio es $${productos[3].precio}`
-	);
-	console.log(carrito);
-} else {
-	alert("Debe ingresar una entrada válida");
-	entrada2 = parseInt(prompt("¿Qué producto desea comprar?\n "));
+/**Agregar Pikachu al carrito */
+let btnAddPikachu = document.getElementById("btnAddPikachu");
+btnAddPikachu.addEventListener("click", agregarPikachu);
+function agregarPikachu() {
+	//sumo un objeto al array
+	articulosCarrito.push(new Producto("1", "Pikachu", "1300"));
+	//creo el elemento
+	let addPikachuCarrito = document.createElement("div");
+	//modifico su innerHTML
+	addPikachuCarrito.innerHTML = `<h5>Pikachu</h5>
+		<p>$1300</p>`;
+	//lo agrego al div cuyo ID es carritoHTML
+	carritoHTML.append(addPikachuCarrito);
+}
+
+// Los demás idem al de arriba
+
+/**Agregar Charmander al carrito */
+let btnAddCharmander = document.getElementById("btnAddCharmander");
+btnAddCharmander.addEventListener("click", agregarCharmander);
+function agregarCharmander() {
+	articulosCarrito.push(new Producto("2", "Charmander", "1100"));
+	let addCharmanderCarrito = document.createElement("div");
+	addCharmanderCarrito.innerHTML = `<h5>Charmander</h5>
+		<p>$1100</p>`;
+	carritoHTML.append(addCharmanderCarrito);
+}
+/**Agregar Espeon al carrito */
+let btnAddEspeon = document.getElementById("btnAddEspeon");
+btnAddEspeon.addEventListener("click", agregarEspeon);
+function agregarEspeon() {
+	articulosCarrito.push(new Producto("3", "Espeon", "1000"));
+	let addEspeonCarrito = document.createElement("div");
+	addEspeonCarrito.innerHTML = `<h5>Espeon</h5>
+		<p>$1000</p>`;
+	carritoHTML.append(addEspeonCarrito);
+}
+/**Agregar Eevee al carrito */
+let btnAddEevee = document.getElementById("btnAddEevee");
+btnAddEevee.addEventListener("click", agregarEevee);
+function agregarEevee() {
+	articulosCarrito.push(new Producto("4", "Eevee", "980"));
+	let addEeveeCarrito = document.createElement("div");
+	addEeveeCarrito.innerHTML = `<h5>Eevee</h5>
+		<p>$980</p>`;
+	carritoHTML.append(addEeveeCarrito);
 }
