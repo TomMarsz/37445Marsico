49b044a23b10cd9c741a6c11ab7b5787799f7fff
tomasfmarsico <tomasfmarsico@gmail.com>
2022-07-04 16:47:58 -0300

    optimizando JS

--- js/main.js
+++ js/main.js
@@ -1,76 +1,112 @@
 /**
- * Ecomerce
+ * @PokeStore: Este script contiene funcionalidades para la compra de articulos Pokemón
+ *
+ * @version: v1.0.0
+ * @author: Tomás Mársico
+ *
+ * History:
+ *  - v1.0.0 – Primera entrega
+ *
  */
 
-/**Constructor de productos */
+/** ELEMENTOS */
+let btnAddPikachu = document.getElementById("btnAddPikachu");
+btnAddPikachu.addEventListener("click", addPikachuCarrito);
+
+let btnAddCharmander = document.getElementById("btnAddCharmander");
+btnAddCharmander.addEventListener("click", addCharmanderCarrito);
+
+let btnAddEspeon = document.getElementById("btnAddEspeon");
+btnAddEspeon.addEventListener("click", addEspeonCarrito);
+
+let btnAddEevee = document.getElementById("btnAddEevee");
+btnAddEevee.addEventListener("click", addEeveeCarrito);
+
+let btnComprar = document.getElementById("btnComprar");
+btnComprar.addEventListener("click", comprarCarrito);
+
+let btnVaciar = document.getElementById("btnVaciar");
+btnVaciar.addEventListener("click", vaciarCarrito);
+
+let tablePokemon = document.getElementById("tablePokemon");
+let totalCarrito = document.getElementById("totalCarrito");
+
+/** CONSTRUCTOR DE PRODUCTOS */
 class Producto {
 	constructor(id, nombre, precio) {
 		this.id = id;
 		this.nombre = nombre;
 		this.precio = precio;
 	}
-	resumen() {
-		console.log(`El producto ${this.nombre} cuesta $${this.precio}`);
-	}
 }
-/** Arrays */
+
+/** ARRAY */
 const productos = [];
+let carrito = [];
+
 /** Push de productos al array "productos" */
-const producto1 = productos.push(new Producto("1", "Pikachu", "1300"));
-const producto2 = productos.push(new Producto("2", "Charmander", "1100"));
-const producto3 = productos.push(new Producto("3", "Espeon", "1000"));
-const producto4 = productos.push(new Producto("4", "Eevee", "980"));
+productos.push(new Producto("1", "Pikachu", "1300"));
+productos.push(new Producto("2", "Charmander", "1100"));
+productos.push(new Producto("3", "Espeon", "1000"));
+productos.push(new Producto("4", "Eevee", "980"));
+
+/** Almacenamiento del array dentro del Storage */
+const guardarLocal = (clave, valor) => {
+	localStorage.setItem(clave, valor);
+};
 for (const producto of productos) {
-	producto.resumen();
+	guardarLocal(producto.id, JSON.stringify(producto));
+}
+guardarLocal();
+
+/** LOGICA */
+
+// Agregar a pikachu al carrito
+function addPikachuCarrito() {
+	let producto1 = JSON.parse(localStorage.getItem("1"));
+	carrito.push(producto1);
+	console.log(carrito);
+	let tr = document.createElement("tr");
+	tr.innerHTML = `<td>${producto1.nombre}</td>
+	<td>${producto1.precio}</td>`;
+	tablePokemon.appendChild(tr);
 }
 
-let articulosCarrito = [];
-let carritoHTML = document.getElementById("carritoHTML");
-
-/**Agregar Pikachu al carrito */
-let btnAddPikachu = document.getElementById("btnAddPikachu");
-btnAddPikachu.addEventListener("click", agregarPikachu);
-function agregarPikachu() {
-	//sumo un objeto al array
-	articulosCarrito.push(new Producto("1", "Pikachu", "1300"));
-	//creo el elemento
-	let addPikachuCarrito = document.createElement("div");
-	//modifico su innerHTML
-	addPikachuCarrito.innerHTML = `<h5>Pikachu</h5>
-		<p>$1300</p>`;
-	//lo agrego al div cuyo ID es carritoHTML
-	carritoHTML.append(addPikachuCarrito);
+// Agregar a charmander al carrito
+function addCharmanderCarrito() {
+	let producto2 = JSON.parse(localStorage.getItem("2"));
+	carrito.push(producto2);
+	let tr = document.createElement("tr");
+	tr.innerHTML = `<td>${producto2.nombre}</td>
+	<td>${producto2.precio}</td>`;
+	tablePokemon.appendChild(tr);
 }
 
-// Los demás idem al de arriba
+// Agregar a espeon al carrito
+function addEspeonCarrito() {
+	let producto3 = JSON.parse(localStorage.getItem("3"));
+	carrito.push(producto3);
+	let tr = document.createElement("tr");
+	tr.innerHTML = `<td>${producto3.nombre}</td>
+	<td>${producto3.precio}</td>`;
+	tablePokemon.appendChild(tr);
+}
 
-/**Agregar Charmander al carrito */
-let btnAddCharmander = document.getElementById("btnAddCharmander");
-btnAddCharmander.addEventListener("click", agregarCharmander);
-function agregarCharmander() {
-	articulosCarrito.push(new Producto("2", "Charmander", "1100"));
-	let addCharmanderCarrito = document.createElement("div");
-	addCharmanderCarrito.innerHTML = `<h5>Charmander</h5>
-		<p>$1100</p>`;
-	carritoHTML.append(addCharmanderCarrito);
+// Agregar a eevee al carrito
+function addEeveeCarrito() {
+	let producto4 = JSON.parse(localStorage.getItem("4"));
+	carrito.push(producto4);
+	let tr = document.createElement("tr");
+	tr.innerHTML = `<td>${producto4.nombre}</td>
+	<td>${producto4.precio}</td>`;
+	tablePokemon.appendChild(tr);
 }
-/**Agregar Espeon al carrito */
-let btnAddEspeon = document.getElementById("btnAddEspeon");
-btnAddEspeon.addEventListener("click", agregarEspeon);
-function agregarEspeon() {
-	articulosCarrito.push(new Producto("3", "Espeon", "1000"));
-	let addEspeonCarrito = document.createElement("div");
-	addEspeonCarrito.innerHTML = `<h5>Espeon</h5>
-		<p>$1000</p>`;
-	carritoHTML.append(addEspeonCarrito);
+
+function vaciarCarrito() {
+	tablePokemon.innerHTML = "";
+	totalCarrito.innerText = "Total: ";
 }
-/**Agregar Eevee al carrito */
-let btnAddEevee = document.getElementById("btnAddEevee");
-btnAddEevee.addEventListener("click", agregarEevee);
-function agregarEevee() {
-	articulosCarrito.push(new Producto("4", "Eevee", "980"));
-	let addEeveeCarrito = document.createElement("div");
-	addEeveeCarrito.innerHTML = `<h5>Eevee</h5>
-		<p>$980</p>`;
-	carritoHTML.append(addEeveeCarrito);
+
+function comprarCarrito() {
+	alert(`Usted compro `);
 }
